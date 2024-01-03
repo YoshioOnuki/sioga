be5cc56fe20879868582c32e698253a9a2f42138
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2024-01-02 21:02:54 -0500

    Refactor code to improve performance and readability

--- resources/views/livewire/inicio/index.blade.php
+++ resources/views/livewire/inicio/index.blade.php
@@ -15,6 +15,7 @@
             </div>
         </div>
     </div>
+
     <div class="page-body">
         <div class="container-xl">
             <div class="card card-md card-stacked animate__animated animate__fadeIn animate__faster">
@@ -29,7 +30,7 @@
                 </div>
                 <div class="card-body">
                     <div class="row align-items-center">
-                        <div class="col-10">
+                        <div class="col-12">
                             <h3 class="h1">Bienvenido</h3>
                             <div>
                                 <p class="text-justify">
@@ -38,7 +39,7 @@
                                     herramientas esenciales para gestionar tu proyecto de grado. Con SIOGA, puedes
                                     llevar a cabo tu investigación académica de manera online, accediendo de forma
                                     directa a los procesos de presentación, revisión, evaluación y aprobación de tu
-                                    propuesta y borradores de tesis.
+                                    propuesta de tesis.
                                 </p>
                                 <strong>RECOMENDACIONES:</strong>
                                 <div class="list-group list-group-flush list-group-hoverable">
@@ -46,8 +47,8 @@
                                         <div class="row align-items-center">
                                             <div class="col-auto"><span class="badge bg-azure"></span></div>
                                             <div class="col text-truncate">
-                                                <div class="text-reset d-block">Utiliza las Herramientas del Tesista:</div>
-                                                <div class="d-block text-secondary text-truncate">Encuentra recursos útiles en esta sección para apoyar tu investigación.</div>
+                                                <div class="text-reset d-block">Define claramente tu idea o tema de investigación:</div>
+                                                <div class="d-block text-secondary text-truncate">Un buen punto de partida es esencial.</div>
                                             </div>
                                         </div>
                                     </div>
@@ -68,8 +69,8 @@
                                         <div class="row align-items-center">
                                             <div class="col-auto"><span class="badge bg-azure"></span></div>
                                             <div class="col text-truncate">
-                                                <div class="text-reset d-block">Utiliza las Herramientas del Tesista:</div>
-                                                <div class="d-block text-secondary text-truncate">Encuentra recursos útiles en esta sección para apoyar tu investigación.</div>
+                                                <div class="text-reset d-block">Identifica tu Línea de Investigación:</div>
+                                                <div class="d-block text-secondary text-truncate">Asegúrate de que tu tema esté alineado con una de las líneas de investigación reconocidas por la universidad.</div>
                                             </div>
                                         </div>
                                     </div>
@@ -79,36 +80,178 @@
                                         <div class="row align-items-center">
                                             <div class="col-auto"><span class="badge bg-azure"></span></div>
                                             <div class="col text-truncate">
-                                                <div class="text-reset d-block">Utiliza las Herramientas del Tesista:</div>
-                                                <div class="d-block text-secondary text-truncate">Encuentra recursos útiles en esta sección para apoyar tu investigación.</div>
+                                                <div class="text-reset d-block">Selecciona tu Asesor de tesis:</div>
+                                                <div class="d-block text-secondary text-truncate">Podrás elegir tu asesor, mientras que los tres jurados de tesis serán asignados aleatoriamente por la comisión de grados y títulos a través de SIOGA.</div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <ul>
-                                    <li>Define claramente tu idea o tema de
-                                        investigación: Un buen punto de partida es
-                                        esencial.</li>
-                                    <li>Utiliza las Herramientas del Tesista: Encuentra recursos útiles en esta sección
-                                        para apoyar tu investigación.</li>
-                                    <li>Identifica tu Línea de Investigación: Asegúrate de que tu tema esté alineado con
-                                        una de las líneas de investigación reconocidas por la universidad.</li>
-                                    <li>Selecciona tu Asesor de tesis: Podrás elegir tu asesor, mientras que los tres
-                                        jurados de tesis serán asignados aleatoriamente por la comisión de grados y
-                                        títulos a través de SIOGA.</li>
-                                </ul>
-
                             </div>
-                            <div class="markdown text-secondary">
+                            <div class="text-justify">
                                 Para optimizar tu experiencia en SIOGA, te recomendamos leer el manual del tesista. <a href="#">Haz clic aquí</a> para más información.
                                 <br> Sigue estas pautas para navegar el proceso académico con claridad y eficacia.
                                 ¡SIOGA
                                 está aquí para apoyarte en cada paso hacia tu logro académico!
                             </div>
-                            <div class="mt-3">
-                                <a href="" class="btn btn-primary" target="_blank">Buy all emails
-                                    for $29</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-body">
+        <div class="container-xl">
+            <div class="card card-md card-stacked animate__animated animate__fadeIn animate__faster">
+                <div class="card-stamp card-stamp-lg">
+                    <div class="card-stamp-icon bg-success">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-report" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M8 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h5.697" />
+                            <path d="M18 14v4h4" />
+                            <path d="M18 11v-4a2 2 0 0 0 -2 -2h-2" />
+                            <path d="M8 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+                            <path d="M18 18m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+                            <path d="M8 11h4" />
+                            <path d="M8 15h3" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row align-items-center">
+                        <div class="col-12">
+                            <h3 class="h1">Procesos</h3>
+
+                            <div class="accordion" id="accordionTimeline">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                            Mostrar procesos ya realizados
+                                        </button>
+                                    </h2>
+                                    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo">
+                                        <div class="accordion-body">
+                                            <div class="timeline-item">
+                                                <ul class="timeline">
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon bg-green-lt">
+                                                            <strong>1</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">01/31/2024 2:30 PM</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon  bg-green-lt">
+                                                            <strong>2</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Hace 3 horas</div>
+                                                                <h4>Proyecto de Tesis en Asesor</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por el Asesor</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button class="accordion-button" type="button">
+                                            Procesos a seguir
+                                        </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionTimeline">
+                                        <div class="accordion-body">
+                                            <div class="timeline-item">
+                                                <ul class="timeline">
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon">
+                                                            <strong>3</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Pendiente</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon">
+                                                            <strong>4</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Pendiente</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon">
+                                                            <strong>5</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Pendiente</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon">
+                                                            <strong>6</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Pendiente</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon">
+                                                            <strong>7</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Pendiente</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="timeline-event">
+                                                        <div class="timeline-event-icon">
+                                                            <strong>8</strong>
+                                                        </div>
+                                                        <div class="card timeline-event-card">
+                                                            <div class="card-body">
+                                                                <div class="text-secondary float-end">Pendiente</div>
+                                                                <h4>Revisión de Formato</h4>
+                                                                <p class="text-secondary">Proyecto de Tesis en revisión por la Comisión de Grados y Títulos.</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
