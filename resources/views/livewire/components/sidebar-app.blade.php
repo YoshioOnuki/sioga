ec84176f2a8341ae357a41457e2e98c0d70a563e
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-01-06 21:05:53 -0500

    Refactor sidebar-app.blade.php and add proyecto link

--- resources/views/livewire/components/sidebar-app.blade.php
+++ resources/views/livewire/components/sidebar-app.blade.php
@@ -10,8 +10,7 @@
                     class="navbar-brand-image rounded hide-theme-light">
                 <img src="{{ asset('media/logo-light.PNG') }}" alt="Logo Unia"
                     class="navbar-brand-image rounded hide-theme-dark"> --}}
-                <img src="{{ asset('media/logo-dark.PNG') }}" alt="Logo Unia"
-                    class="navbar-brand-image rounded">
+                <img src="{{ asset('media/logo-dark.PNG') }}" alt="Logo Unia" class="navbar-brand-image rounded">
                 <span class="text-uppercase" style="font-weight: 800; font-size: 1.8rem;">
                     SIOGA
                 </span>
@@ -176,6 +175,32 @@
                             </div>
                         </div>
                     </li>
+                @endif
+                @if (auth()->user()->permiso('proyecto-index'))
+                <li class="nav-item {{ request()->routeIs('proyecto*') ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('proyecto') }}">
+                        <span class="nav-link-icon d-md-none d-lg-inline-block">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path
+                                    d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+                                <path
+                                    d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" />
+                                <path d="M5 8h4" />
+                                <path d="M9 16h4" />
+                                <path
+                                    d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z" />
+                                <path d="M14 9l4 -1" />
+                                <path d="M16 16l3.923 -.98" />
+                            </svg>
+                        </span>
+                        <span class="nav-link-title">
+                            Proyectos
+                        </span>
+                    </a>
+                </li>
                 @endif
                 <hr class="ms-lg-3 mt-3 mb-3">
             </ul>
