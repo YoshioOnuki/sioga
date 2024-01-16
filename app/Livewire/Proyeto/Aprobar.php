f585e4c8a1c13277f4cb2720b90867b4a849690b
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-01-15 19:12:59 -0500

    Component Aprovar y Revisar Proyectos

---
+++ app/Livewire/Proyeto/Aprobar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Proyeto;
+
+use Livewire\Component;
+
+class Aprobar extends Component
+{
+    public function render()
+    {
+        return view('livewire.proyeto.aprobar');
+    }
+}
---
+++ app/Livewire/Proyeto/Revisar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Proyeto;
+
+use Livewire\Component;
+
+class Revisar extends Component
+{
+    public function render()
+    {
+        return view('livewire.proyeto.revisar');
+    }
+}
